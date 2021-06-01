2910c6b4cce274f3547bce671cadf0b990937e19
mvgarcia <valentina260101@gmail.com>
2021-05-31 22:01:20 -0500

    Inicial

---
+++ src/ProblemaA/ProblemaA.java
@@ -0,0 +1,10 @@
+package ProblemaA;
+
+public class ProblemaA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ProblemaB/ProblemaB.java
@@ -0,0 +1,10 @@
+package ProblemaB;
+
+public class ProblemaB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
