f67917d2c53b462b739597908aad5f36fe7437cb
mvgarcia <valentina260101@gmail.com>
2021-06-02 21:54:22 -0500

    Avance

--- src/ProblemaA/ProblemaA.java
+++ src/ProblemaA/ProblemaA.java
@@ -1,10 +1,178 @@
 package ProblemaA;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+//Autores: Pedro Vallejo, Valentina Garcia
+
 public class ProblemaA {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public ProblemaA()
+	{
+		
+	}
+	
+	public int[][] calculoPotencia(int[][] A, int r, int m)
+	{
+		int [][] s = new int[A.length][A.length];
+		
+		String binary = Integer.toBinaryString(r);
+		
+		int[][][] parcial = new int [binary.length()][A.length][A.length];
+		parcial[binary.length()-1]=A;
+		
+		int it=1;
+		for(int z=binary.length()-2;z>=0 && it<r;z--)
+		{
+			it*=2;
+			parcial[z]=multMatrix(parcial[z+1], parcial[z+1],m);
+		}
+		
+		s=parcial[0];
+		
+		for(int i=1;i<binary.length();i++)
+		{
+			if(binary.charAt(i)=='1')s=multMatrix(s, parcial[i],m);
+		}
+		
+		
+//		if(zm%2==0)
+//		{
+//			int[][] m2=multMatrix(A, A);
+//			int[][] last=m2;
+//			for(int z=2;z<zm;z+=2)
+//			{
+//				last=multMatrix(last, m2);
+//			}
+//			r=last;
+//		}
+//		else
+//		{
+//			int[][] m2=multMatrix(A, A);
+//			int[][] last=m2;
+//			for(int z=2;z<zm-1;z+=2)
+//			{
+//				last=multMatrix(last, m2);
+//			}
+//			r=multMatrix(last, A);
+//		}
+		
+//		for(int z=1;z<=zm;z++)
+//		{
+//			int prime= maxPrime(z);
+//			if(z==1)
+//			{
+//				parcial[z-1]=A;
+//			}
+//			else if(prime==z)
+//			{
+//				parcial[z-1]=multMatrix(parcial[z-2],A);
+//			}
+//			
+//			else
+//			{
+//				parcial[z-1]=multMatrix(parcial[prime-1],parcial[(z/prime)-1]);
+//			}
+		
+//			if(z==1)
+//			{
+//				parcial[z-1]=A;
+//			}
+//			else
+//			{
+//				parcial[z-1]=multMatrix(parcial[z-2],A);
+//			}
+			
+			
+			
+//		}
+//		r=parcial[zm-1];
+		
+		return s;
 	}
 
+	public int[][] multMatrix(int[][] A, int[][]B, int m)
+	{
+		int [][] s = new int[A.length][A.length];
+
+		for (int i=0; i<A.length;i++)
+		{
+			for (int j=0; j<B.length;j++)
+			{
+				
+				for (int k=0; k<A.length;k++)
+				{
+					s[i][j]+=A[i][k]*B[k][j];
+				}
+				
+				s[i][j]=s[i][j]%m;
+			}
+		}
+
+		return s;
+	}
+
+//	public static void main(String[] args) {
+//		
+//		ProblemaA p= new ProblemaA();
+//		
+//		int n=50;
+//		int [][]A = new int[n][n];
+//		for(int i=0;i<n;i++)
+//		{
+//			for(int j=0;j<n;j++) {A[i][j]=i+j;System.out.print(A[i][j]+" ");
+//			}
+//			System.out.println();
+//		}
+//		
+//		int [][]r = new int[n][n];
+//		int zm=1000;
+//		long startTime = System.currentTimeMillis();
+//		int m=1000;
+//		r=p.calculoPotencia(A, zm,m);
+//		long endTime = System.currentTimeMillis();
+//		for(int i=0;i<n;i++)
+//		{
+//			for(int j=0;j<n;j++)System.out.print(r[i][j]+" ");
+//			System.out.println();
+//		}
+//		System.out.println("Total time spent (milliseconds): "+(endTime-startTime));
+//
+//	}
+	
+	public static void main(String[] args) throws Exception {
+		ProblemaA instancia = new ProblemaA();
+		try ( 
+			InputStreamReader is= new InputStreamReader(System.in);
+			BufferedReader br = new BufferedReader(is);
+		) { 
+			String line = br.readLine();
+			
+			while(line!=null && line.length()>0 && !"0 0 0".equals(line)) {
+				final String [] dataStr = line.split(" ");
+				int n=Integer.parseInt(dataStr[0]);
+				int r= Integer.parseInt(dataStr[1]);
+				int m=Integer.parseInt(dataStr[2]);
+				int[][]A=new int[n][n];
+				
+				for(int i=0;i<n;i++)
+				{
+					line = br.readLine();
+					final String [] dataStr1 = line.split(" ");
+					final int[] fila = Arrays.stream(dataStr1).mapToInt(f->Integer.parseInt(f)).toArray();
+					A[i]=fila;
+				}
+				
+				int [][] s = instancia.calculoPotencia(A, r, m);
+				System.out.println();
+				for(int i=0;i<n;i++)
+				{
+					for(int j=0;j<n;j++)System.out.print(s[i][j]+" ");
+					if(i!=n-1)System.out.println();
+				}
+				line = br.readLine();
+			}
+		}
+	}
 }
--- src/ProblemaB/ProblemaB.java
+++ src/ProblemaB/ProblemaB.java
@@ -1,6 +1,92 @@
 package ProblemaB;
 
+
+import java.util.ArrayDeque;
 public class ProblemaB {
+	
+	//Autores: Pedro Vallejo, Valentina Garcia
+	
+	public int balanceoCarga(int[]t, int p)
+	{
+		int camin=0;
+		int tamanio= Math.floorDiv(t.length, p);
+		int res=t.length%p;
+		if(res>0)tamanio+=1;
+		ArrayDeque<Integer>[] a = new ArrayDeque[p];		
+		
+		int i=0;
+		int[] valores = new int[p];
+		for(int j=0;j<p && i<t.length;j++)
+		{
+			int cota=0;
+			if(res>0) {cota= tamanio+1;res--;}
+			else {cota=tamanio;}
+			for(int k=0;k<cota;k++)
+			{
+				a[j].addLast(t[i]);
+				valores[j]+=t[i];
+				i++;
+			}
+		}
+		
+		boolean subio= false;
+		while(subio)
+		{
+			int [] maxs=darMax(valores);
+			int max=maxs[0];
+			int posMax=maxs[1];
+			if(posMax==0)
+			{
+				int cambio=a[posMax+1].getFirst()+a[posMax].getLast();
+				valores[posMax]-=cambio;
+				valores[posMax+1]+=cambio;
+				a[posMax+1].addFirst(a[posMax].removeLast());
+			}
+			else if(posMax==valores.length-1)
+			{
+				int cambio=a[posMax].getFirst()+a[posMax-1].getLast();
+				valores[posMax-1]+=cambio;
+				valores[posMax]-=cambio;
+				a[posMax-1].addLast(a[posMax].removeFirst());
+			}
+			else
+			{
+				int menor=valores[posMax-1];
+				if(valores[posMax+1]<menor)menor=valores[posMax+1];
+				if(menor==valores[posMax-1])
+				{
+					int cambio=a[posMax].getFirst()+a[posMax-1].getLast();
+					valores[posMax-1]+=cambio;
+					valores[posMax]-=cambio;
+					a[posMax-1].addLast(a[posMax].removeFirst());
+				}
+				else
+				{
+					int cambio=a[posMax+1].getFirst()+a[posMax].getLast();
+					valores[posMax]-=cambio;
+					valores[posMax+1]+=cambio;
+					a[posMax+1].addFirst(a[posMax].removeLast());
+				}
+			}
+			int nuevoMax=darMax(valores)[0];
+			if(nuevoMax>max)subio=true;
+			camin=max;
+		}
+		
+		return camin;
+	}
+	
+	public int[] darMax(int[]valores)
+	{
+		int [] max=new int[2];
+		for(int v=0;v<valores.length;v++) {
+			if(v>max[0]) {
+				max[0]=valores[v];
+				max[1]=v;
+			}
+		}
+		return max;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
